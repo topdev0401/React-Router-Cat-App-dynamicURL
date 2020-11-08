6f29c93d37344ef35040e8773e00a978f1a90e95
topdev0401 <helloworld0401@outlook.com>
2020-11-08 20:01:38 +0100

    fixes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -24,7 +24,7 @@ class Navbar extends Component {
         aria-controls="navbarNav" 
         aria-expanded="false" 
         aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"/>
+        <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
